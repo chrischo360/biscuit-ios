51fe399fc387f79abc2134fa4d50f1fd38dc870e
chrischo <chrischo@bu.edu>
2023-12-05 12:45:35 -0500

    first commit

---
+++ biscuit-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  biscuit-ios
+//
+//  Created by Chris Cho on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ biscuit-ios/biscuit_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  biscuit_iosApp.swift
+//  biscuit-ios
+//
+//  Created by Chris Cho on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct biscuit_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
